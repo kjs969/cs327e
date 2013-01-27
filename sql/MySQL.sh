8000b69fa77d2691767f72e2ad1a4d5d9a255eea
Glenn Downing <downing@cs.utexas.edu>
2013-01-27 16:13:44 -0600

    another commit

--- sql/MySQL.sh
+++ sql/MySQL.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-mysql -h z -u <username> -p<password> -v -H < listing0002.sql > listing0002.html
+mysql -h z -u <username> -p<password> -v -H < listing0101.sql > listing0101.html
