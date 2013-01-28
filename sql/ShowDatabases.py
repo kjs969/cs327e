b047a1a1e17fb0a23848140be82a3516c23defcb
Glenn Downing <downing@cs.utexas.edu>
2013-01-27 22:12:46 -0600

    another commit

--- sql/ShowDatabases.py
+++ sql/ShowDatabases.py
@@ -8,28 +8,19 @@ import Login
 import Query
 
 print "ShowDatabases.py"
-print
 
 c = Login.login()
 t = Query.query(c, "show databases")
-print t
-print
+assert t == \
+    (('information_schema',), \
+     ('downing',), \
+     ('downing_cs327e',), \
+     ('downing_cs370',), \
+     ('downing_cs371p',), \
+     ('downing_cs373',), \
+     ('downing_cs378',), \
+     ('downing_dml',), \
+     ('downing_test',), \
+     ('drupal_dml',))
 
 print "Done."
-
-"""
-ShowDatabases.py
-
-(('information_schema',),
- ('downing',),
- ('downing_cs327e',),
- ('downing_cs370',),
- ('downing_cs371p',),
- ('downing_cs373',),
- ('downing_cs378',),
- ('downing_dml',),
- ('downing_test',),
- ('drupal_dml',))
-
-Done.
-"""
