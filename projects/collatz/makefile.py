46be52056738c65c8d7a0a4b2182b34d6a32d346
Glenn Downing <downing@cs.utexas.edu>
2013-01-29 08:25:35 -0600

    another commit

--- projects/collatz/makefile.py
+++ projects/collatz/makefile.py
@@ -9,8 +9,17 @@ diff:
 test:
 	TestCollatz.py
 
+turnin-list:
+	turnin --list hychyc07 cs372epj1
+
+turnin-submit:
+	turnin --submit hychyc07 cs372epj1 Collatz.zip
+
+turnin-verify:
+	turnin --verify hychyc07 cs372epj1
+
 zip:
-	zip -r Collatz.zip EADME.txt makefile Collatz.html Collatz.log Collatz.py \
+	zip -r Collatz.zip README.txt makefile Collatz.html Collatz.log Collatz.py \
 	RunCollatz.in RunCollatz.out RunCollatz.py SphereCollatz.py \
 	TestCollatz.py TestCollatz.out
 
