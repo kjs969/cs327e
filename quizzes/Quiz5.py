fff0794952d3904f080491bfde84e852cbfe6573
Glenn Downing <downing@cs.utexas.edu>
2013-01-30 20:12:12 -0600

    another commit

---
+++ quizzes/Quiz5.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+"""
+CS327e: Quiz #5 (5 pts)
+"""
+
+""" ----------------------------------------------------------------------
+1. Tables have an attractive property called "closure". Explain it.
+   [SQL: Ch.2, Pg. 36]
+   (2 pts)
+
+any operation performed on a table yields another table
+"""
+
+""" ----------------------------------------------------------------------
+2. Which of the following practices demonstrates effective pair
+   programming?
+   [All I Really Needed to Know about Pair Programming I Learned in
+    Kindergarten]
+   (2 pts)
+
+a. Each partner writing separate parts.
+b. Each partner writing both parts and then submitting the best.
+c. Each partner writing both parts and then submitting the best
+   combination.
+d. Sharing a monitor and keyboard while coding.
+e. One partner writing the interface and tests, the other the
+   implementation.
+
+d.
+"""
