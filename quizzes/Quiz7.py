c2c7e685f586bae8402ab9a01b5a4b9ecf4e857b
Glenn Downing <downing@cs.utexas.edu>
2013-02-04 18:27:17 -0600

    another commit

---
+++ quizzes/Quiz7.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+"""
+CS327e: Quiz #7 (5 pts)
+"""
+
+""" ----------------------------------------------------------------------
+1. Which of the following is true about strong aggregation, also known as
+   composition?
+   [UML Tutorial]
+   (2 pts)
+
+a. filled diamond
+   lifetime of the part is dependent on lifetime of the whole
+b. filled diamond
+   lifetime of the part is not dependent on lifetime of the whole
+c. no diamond
+   no dependency between lifetime of the part and lifetime of the whole
+d. unfilled diamond
+   lifetime of the part is dependent on lifetime of the whole
+e. unfilled diamond
+   lifetime of the part is not dependent on lifetime of the whole
+
+a.
+"""
+
+""" ----------------------------------------------------------------------
+2. How would the value "123.89" display with the format NUMERIC(3,1)?
+   [SQL: Ch.3, Pg. 74]
+   (2 pts)
+
+a. <exceeds precsison>
+b. 123
+c. 123.8
+d. 123.9
+e. 124
+
+a.
+"""
