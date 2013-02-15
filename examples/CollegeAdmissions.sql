8681259bd2ee61bc080f7026d9f50c020c3d9dfe
Glenn Downing <downing@cs.utexas.edu>
2013-02-14 21:15:08 -0600

    another commit

--- examples/CollegeAdmissions.sql
+++ examples/CollegeAdmissions.sql
@@ -8,11 +8,6 @@ drop table if exists Apply;
 
 # create
 
-create table College (
-	cName      text,
-	state      char(2),
-	enrollment int);
-
 create table Student (
 	sID    int,
 	sName  text,
@@ -24,6 +19,11 @@ create table Apply (
 	cName    text,
 	major    text,
 	decision boolean);
+
+create table College (
+	cName      text,
+	state      char(2),
+	enrollment int);
 
 # insert
 
@@ -39,12 +39,6 @@ insert into Student values (876, 'Irene',  3.9,  400);
 insert into Student values (765, 'Jay',    2.9, 1500);
 insert into Student values (654, 'Amy',    3.9, 1000);
 insert into Student values (543, 'Craig',  3.4, 2000);
-
-insert into College values ('Stanford', 'CA', 15000);
-insert into College values ('Berkeley', 'CA', 36000);
-insert into College values ('MIT',      'MA', 10000);
-insert into College values ('Cornell',  'NY', 21000);
-insert into College values ('Irene',    'NY', 21000);
 
 insert into Apply values (123, 'Stanford', 'CS',             true);
 insert into Apply values (123, 'Stanford', 'EE',             false);
@@ -66,56 +60,68 @@ insert into Apply values (765, 'Cornell',  'history',        false);
 insert into Apply values (765, 'Cornell',  'psychology',     true);
 insert into Apply values (543, 'MIT',       'CS',            false);
 
+insert into College values ('Stanford', 'CA', 15000);
+insert into College values ('Berkeley', 'CA', 36000);
+insert into College values ('MIT',      'MA', 10000);
+insert into College values ('Cornell',  'NY', 21000);
+insert into College values ('Irene',    'NY', 21000);
+
 # show
 
 show tables;
 show columns from Student;
-show columns from College;
 show columns from Apply;
+show columns from College;
 
 # select
 
-select * from College;
 select * from Student;
 select * from Apply;
+select * from College;
 
 # select
 
+# students with GPA > 3.7
 # select[GPA > 3.7](Student)
 select * from Student where (GPA > 3.7);
 
+# students with GPA > 3.7 with high school size < 1000
 # select[(GPA > 3.7) and (sizeHS < 1000)](Student)
 select * from Student where (GPA > 3.7) and (sizeHS < 1000);
 
+# applications to Stanford with major = CS
 # select[(GPA > 3.7) and (sizeHS < 1000) and (major = 'CS')](Student)
 select * from Apply where (cName = 'Stanford') and (major = 'CS');
 
 # project
 
+# student ID and decision of applications
 # project[sID, decision](Apply)
 select sID, decision from Apply;
 
 # select and project
 
-# project[sID, sName, GPA](select[GPA > 3.7](Student)
-select sID, sName, GPA from Student where (GPA > 3.7);
+# ID and name of students with GPA > 3.7
 
-# project[sID, sName](select[GPA > 3.7](Student)
+# project[sID, sName](select[GPA > 3.7](Student))
 select sID, sName from Student where (GPA > 3.7);
 
 # select distinct
 
+# major and decision of applications
 # project[major, decision](Apply)
 select          major, decision from Apply;
 select distinct major, decision from Apply;
 
 # cross join
 
+# Student cross Apply
 select *
 	from Student cross join Apply;
 
 # inner join (theta join)
 
+# Student join[Student.sID = Apply.sID] Apply
 select *
 	from Student, Apply
 	where (Student.sID = Apply.sID);
@@ -128,44 +134,44 @@ select *
 	from Student inner join Apply
 	using (sID);
 
-# natural join
-
 select *
 	from Student natural join Apply;
 
-# names and GPAs of Students with
-#   high school size > 1000,
-#   who applied to CS,
-#   and were rejected
+# name and GPA of students
+#   with high school size > 1000,
+#   with major            = CS,
+#   with decision         = false
 
+# select[(sizeHS > 1000) and (major = 'CS') and (decision = false)]
+# 	(Student join Apply)
 select *
-	from Student inner join Apply
-	    on (Student.sID = Apply.sID)
+	from Student natural join Apply
 	where (sizeHS > 1000) and (major = 'CS') and (decision = false);
 
+# project[sName, GPA]
+# 	select[(sizeHS > 1000) and (major = 'CS') and (decision = false)]
+# 		(Student join Apply)
 select sName, GPA
 	from Student natural join Apply
 	where (sizeHS > 1000) and (major = 'CS') and (decision = false);
 
-# names and GPAs of Students with
-#   high school size > 1000,
-#   who applied to CS,
-#   and were rejected
-#   at a college with enrollment > 20000,
+# name and GPA of students with
+#   with high school size > 500,
+#   with major            = CS,
+#   with decision         = false,
+#   with enrollment       > 20000
 
+# select[(sizeHS > 500) and (major = 'CS') and (decision = false) and (enrollment > 20000)]
+# 	(Student join Apply join College)
 select *
-	from Student
-	    inner join Apply   on (Student.sID   = Apply.sID)
-	    inner join College on (College.cName = Apply.cName)
+	from Student natural join Apply natural join College
 	where (sizeHS     > 500)   and
 	      (major      = 'CS')  and
 	      (decision   = true)  and
 	      (enrollment > 20000);
 
 select sName, GPA
-	from Student
-	    natural join Apply
-	    natural join College
+	from Student natural join Apply natural join College
 	where (sizeHS     > 500)   and
 	      (major      = 'CS')  and
 	      (decision   = true)  and
@@ -173,94 +179,151 @@ select sName, GPA
 
 # set union
 
-# list of College names and Student names
+# names of students and colleges
+
+# project[cName](Student) union project[sName](College)
 (select cName from College)
 union
 (select sName from Student);
 
-(select cName as csName from College)
+(select cName as A1 from College)
 union
-(select sName as csName from Student);
+(select sName as A1 from Student);
 
 # using temporary tables
 
-create temporary table T1 as select cName as csName from College;
-create temporary table T2 as select sName as csName from Student;
-(select * from T1) union (select * from T2);
+# T1a := select[cName](College)
+create temporary table T1a as select cName as A1 from College;
+
+# T1b := select[sName](Student)
+create temporary table T1b as select sName as A1 from Student;
+
+# T1a join T1b
+(select * from T1a) union (select * from T1b);
 
 # set difference
 
-# ids of Students who didn't Apply anywhere
-# (select sID from Student) except (select sID from Apply);
-# MySQL doesn't support 'except'
+# ID and name of students who didn't apply anywhere
 
-select * from Student
-	where not exists
-	    (select sID from Apply where (Student.sID = Apply.sID));
-
-select distinct sID from Student
-	where not exists
-	    (select sID from Apply where (Student.sID = Apply.sID));
-
-# ids and names of Students who didn't Apply anywhere
-
-create temporary table T3 as
+# T2a := project[sID](Student) diff project[sID](Apply)
+create temporary table T2a as
 	select distinct sID from Student
 	    where not exists
 	        (select sID from Apply where (Student.sID = Apply.sID));
+
+# T2a join Student
 select * from
-	T3 natural join Student;
+	T2a natural join Student;
+
+# project[sID, sName](T2a join Student)
 select sID, sName from
-	T3 natural join Student;
+	T2a natural join Student;
 
 # set intersection
 
-# names that are both a College name and a Student name
-# (select cName from College) intersect (select sName from Student);
-# MySQL doesn't support 'intersect'
+# names that are both a student name and a college name
 
-select distinct cName as xName from College
+# project[sName](Student) intersect project[cName](College)
+select cName from College
 	where exists
 	    (select sName from Student where (College.cName = Student.sName));
 
-create temporary table T5 as
-	select distinct cName as xName from College;
-create temporary table T6 as
-	select distinct sName as xName from Student;
+# T3a := rename[A1](project[sName](Student))
+create temporary table T3a as
+	select sName as A1 from Student;
+
+# T3b := rename[A1](project[cName](College))
+create temporary table T3b as
+	select cName as A1 from College;
+
+# T3a join T3b
 select * from
-	T5 natural join T6;
-select xName from
-	T5 natural join T6;
+	T3a natural join T3b;
 
-# pairs of Colleges in the same state
+# pairs of names of colleges in the same state
 
-select distinct A1.cName, A2.cName from
-	College as A1 cross join College as A2
-	where A1.cName != A2.cName and A1.state = A2.state;
-
-select distinct A1.cName, A2.cName from
-	College as A1 cross join College as A2
-	where A1.cName < A2.cName and A1.state = A2.state;
-
-create temporary table T7 as
+# T5a := rename[cName1, state, enrollment1](College);
+create temporary table T5a as
 	select cName as cName1, state, enrollment as enrollment1 from College;
-create temporary table T8 as
+
+# T5b := rename[cName2, state, enrollment2](College);
+create temporary table T5b as
 	select cName as cName2, state, enrollment as enrollment2 from College;
+
+# project[cName1, cName2](select[cName1 != cName2](T5a join T5b))
 select cName1, cName2 from
-	T7 natural join T8
+	T5a natural join T5b
+	where cName1 != cName2;
+
+# project[cName1, cName2](select[cName1 < cName2](T5a join T5b))
+select cName1, cName2 from
+	T5a natural join T5b
 	where cName1 < cName2;
 
-# gpas of Students Applying to cs in ca
+# gpas of students applying to cs in ca
 
-select GPA from Student
-	natural join College
-	natural join Apply
-	where state = "CA" and major = "CS";
+# select[(major = 'CS') and (state = 'CA')](Apply join Student)
+select *
+	from Student natural join Apply natural join College
+	where major = "CS" and state = "CA";
+
+# project[GPA](select[(major = 'CS') and (state = 'CA')](Apply join Student))
+select GPA
+	from Student natural join Apply natural join College
+	where major = "CS" and state = "CA";
+
+# colleges with enrollments < 20000 that Amy or Irene applied to
+
+# select[((sName = 'Amy') or (sName = 'Irene')) and (enrollment < 20000)]
+# 	(Student join Apply join College)
+select * from
+	Student natural join Apply natural join College
+	where ((sName = 'Amy') or (sName = 'Irene') and (enrollment < 20000));
+
+# project[cName](
+# 	select[((sName = 'Amy') or (sName = 'Irene')) and (enrollment < 20000)]
+# 	(Student join Apply join College))
+
+select distinct cName from
+	Student natural join Apply natural join College
+	where (((sName = 'Amy') or (sName = 'Irene')) and (enrollment < 20000));
+
+# colleges with enrollments < 20000 that Amy and Irene applied to
+
+# project[cName](
+# 	select[(sName = 'Amy') and (enrollment < 20000)]
+# 	(Student join Apply join College))
+# intersect
+# project[cName](
+# 	select[(sName = 'Irene') and (enrollment < 20000)]
+# 	(Student join Apply join College))
+
+# rename[A1](
+# 	project[cName](
+# 		select[(sName = 'Amy') and (enrollment < 20000)]
+# 		(Student join Apply join College)))
+create temporary table T6a as
+	select distinct cName as A1 from
+		Student natural join Apply natural join College
+		where ((sName = 'Amy') and (enrollment < 20000));
+
+# rename[A1](
+# 	project[cName](
+# 		select[(sName = 'Irene') and (enrollment < 20000)]
+# 		(Student join Apply join College)))
+create temporary table T7a as
+	select distinct cName as A1 from
+		Student natural join Apply natural join College
+		where ((sName = 'Irene') and (enrollment < 20000));
+
+# T6a join T7a
+select *
+	from T6a natural join T7a;
 
 # drop
 
-drop table College;
 drop table Student;
 drop table Apply;
+drop table College;
 
 exit
