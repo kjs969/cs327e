49516fd28acdcfcf1dd92acdc8acea19588d3475
Glenn Downing <downing@cs.utexas.edu>
2013-02-21 11:49:35 -0600

    another commit

--- examples/Join.sql
+++ examples/Join.sql
@@ -30,31 +30,38 @@ select *
     from Student natural join Apply;
 
 # name and GPA of students
-#   with high school size > 1000,
-#   with major            = CS,
-#   with decision         = false
+#    with high school size > 1000,
+#    with major            = CS,
+#    with decision         = false
 
-# select[(sizeHS > 1000) and (major = 'CS') and (decision = false)]
-#   Student join Apply
+# select[(sizeHS > 1000)     and
+#        (major = 'CS')      and
+#        (decision = false)]
+#    Student join Apply
 select *
     from Student natural join Apply
     where (sizeHS > 1000) and (major = 'CS') and (decision = false);
 
 # project[sName, GPA]
-#   select[(sizeHS > 1000) and (major = 'CS') and (decision = false)]
+#    select[(sizeHS > 1000)     and
+#           (major = 'CS')      and
+#           (decision = false)]
 #       Student join Apply
 select sName, GPA
     from Student natural join Apply
     where (sizeHS > 1000) and (major = 'CS') and (decision = false);
 
 # name and GPA of students with
-#   with high school size > 500,
-#   with major            = CS,
-#   with decision         = false,
-#   with enrollment       > 20000
+#    with high school size > 500,
+#    with major            = CS,
+#    with decision         = false,
+#    with enrollment       > 20000
 
-# select[(sizeHS > 500) and (major = 'CS') and (decision = false) and (enrollment > 20000)]
-#   Student join Apply join College
+# select[(sizeHS > 500)        and
+#        (major = 'CS')        and
+#        (decision = false)    and
+#        (enrollment > 20000)]
+#    Student join Apply join College
 select *
     from Student natural join Apply natural join College
     where (sizeHS     > 500)   and
@@ -63,8 +70,11 @@ select *
           (enrollment > 20000);
 
 # project[sName, GPA]
-#   select[(sizeHS > 500) and (major = 'CS') and (decision = false) and (enrollment > 20000)]
-#       Student join Apply join College
+#     select[(sizeHS > 500)       and
+#           (major = 'CS')        and
+#           (decision = false)    and
+#           (enrollment > 20000)]
+#         Student join Apply join College
 select sName, GPA
     from Student natural join Apply natural join College
     where (sizeHS     > 500)   and
--- examples/Subqueries.sql
+++ examples/Subqueries.sql
@@ -6,8 +6,8 @@ use downing_test;
 
 # ID, name, and GPA of students who applied in CS
 
-project[sID, sName, GPA]
-    select[major = 'CS'](Student join Apply)
+# project[sID, sName, GPA]
+#     select[major = 'CS'](Student join Apply)
 
 select sID, sName, GPA
     from Student natural join Apply
@@ -34,6 +34,10 @@ select sID, sName, GPA
 # this doesn't work
 # because it has duplicates
 
+# project GPA
+#     select[major = 'CS']
+#         Student join Apply
+
 select GPA
     from Student natural join Apply
     where major = 'CS'
@@ -49,6 +53,8 @@ select distinct GPA
 
 # this does work
 
+# project[GPA]
+#     Student diff select[major = 'CS']Student
 select GPA
     from Student
     where sID in (
@@ -57,14 +63,15 @@ select GPA
             where major = 'CS')
     order by GPA desc;
 
-# ----------------
-# temporary tables
-# ----------------
-
+# using temporary tables
 # fixed, but cumbersome without subquery
 
+# project[GPA]
+#     project[sID, GPA]
+#         select[major = 'CS]
+#             Student join Apply
 create temporary table T1a as
-    select distinct sID, sName, GPA
+    select distinct sID, GPA
         from Student natural join Apply
         where major = 'CS';
 select GPA
@@ -76,14 +83,24 @@ select GPA
 # this doesn't work
 # because students may be majoring CS in more than one place
 
+# project[sID]
+#     select[(major1 = 'CS') and (major2 != 'EE')]
+#          rename[sID, cName1, major1, decision1] Apply
+#          join
+#          rename[sID, cName2, major2, decision2] Apply
 select distinct R.sID
-    from Apply as R, Apply as S
+    from Apply as R inner join Apply as S
     where R.sID    = S.sID and
           R.major  = 'CS'  and
           S.major != 'EE';
 
 # this works
 
+# project sID
+#     select[major  = 'CS'] Student
+# diff
+# project sID
+#     select[major != 'EE'] Student
 select sID
     from Student
     where
@@ -91,7 +108,7 @@ select sID
             select sID
                 from Apply
                 where major = 'CS')
-    and
+        and
         sID not in (
             select sID
                 from Apply
@@ -99,10 +116,8 @@ select sID
 
 # colleges, such that there's another college in the same state
 
-# --------------------
-# as for renaming
-# exists for existence
-# --------------------
+# using as for renaming
+# using exists for existence
 
 # this doesn't work
 # because a college is in the same state as itself
@@ -125,9 +140,7 @@ select cName, state
 
 # college with highest enrollment
 
-# ---------------------------
-# not exists for nonexistence
-# ---------------------------
+# using not exists for nonexistence
 
 select cName
     from College as R
@@ -145,9 +158,7 @@ select sID, sName, GPA
             from Student as S
             where R.GPA < S.GPA);
 
-# ------------------
-# all for comparison
-# ------------------
+# using all for comparison
 
 select sID, sName, GPA
     from Student
