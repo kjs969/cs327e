30f013393cc7e525bc68d23584e695c1a99242a1
Glenn Downing <downing@cs.utexas.edu>
2013-03-21 22:10:13 -0500

    another commit

--- examples/listing0002.sql
+++
@@ -1,6 +0,0 @@
-# pg. 28
-
-USE downing_test;
-
-SELECT au_fname, au_lname
-  FROM authors;
--- examples/listing0101.sql
+++
@@ -1,7 +0,0 @@
-# pg. 28
-
-USE downing_test;
-
-SELECT au_fname, au_lname
-  FROM authors
-  ORDER BY au_lname;
