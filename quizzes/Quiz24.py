4ddf065d24433e4e550309e1fd90e68972aeb71e
Glenn Downing <downing@cs.utexas.edu>
2013-03-28 14:44:16 -0500

    another commit

---
+++ quizzes/Quiz24.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+"""
+CS327e: Quiz #23 (5 pts)
+"""
+
+""" ----------------------------------------------------------------------
+1. What is the output of the following program?
+   (4 pts)
+
+['a', 'b', [4], (5,)]
+[2, 3, [4], (5,)]
+[2, 3, [4], 6]
+[2, 3, 6, (5,)]
+"""
+
+def f ((x, y), z = [4], t = (5,)) :
+    return [x, y, z, t]
+
+print f("ab")
+print f([2, 3])
+print f((2, 3), t = 6)
+print f([2, 3], 6)
