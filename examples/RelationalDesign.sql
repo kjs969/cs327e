b48dc870b701ab247fc032b204107f4fdcb75471
Glenn Downing <downing@cs.utexas.edu>
2013-04-07 18:11:47 -0500

    another commit

---
+++ examples/RelationalDesign.sql
@@ -0,0 +1,188 @@
+use downing_test;
+
+/* -----------------------------------------------------------------------
+Redundancy
+Update Anomaly: change cornet to trumpet
+Deletion Anomaly: delete surfing
+*/
+
+drop table if exists Apply;
+
+create table Apply (
+    SSN    int,
+    sName  text,
+    cName  text,
+    HS     text,
+    HScity text,
+    hobby  text);
+
+insert into Apply values (123, 'Ann', 'Stanford', 'PAHS', 'Palo Alto', 'tennis');
+insert into Apply values (123, 'Ann', 'Berkeley', 'PAHS', 'Palo Alto', 'tennis');
+insert into Apply values (123, 'Ann', 'MIT',      'PAHS', 'Palo Alto', 'tennis');
+insert into Apply values (123, 'Ann', 'Stanford', 'GHS',  'Palo Alto', 'tennis');
+insert into Apply values (123, 'Ann', 'Berkeley', 'GHS',  'Palo Alto', 'tennis');
+insert into Apply values (123, 'Ann', 'MIT',      'GHS',  'Palo Alto', 'tennis');
+insert into Apply values (123, 'Ann', 'Stanford', 'PAHS', 'Palo Alto', 'trumpet');
+insert into Apply values (123, 'Ann', 'Berkeley', 'PAHS', 'Palo Alto', 'trumpet');
+insert into Apply values (123, 'Ann', 'MIT',      'PAHS', 'Palo Alto', 'trumpet');
+insert into Apply values (123, 'Ann', 'Stanford', 'GHS',  'Palo Alto', 'trumpet');
+insert into Apply values (123, 'Ann', 'Berkeley', 'GHS',  'Palo Alto', 'trumpet');
+insert into Apply values (123, 'Ann', 'MIT',      'GHS',  'Palo Alto', 'trumpet');
+insert into Apply values (456, 'Bob', 'MIT',      'GHS',  'Palo Alto', 'surfing');
+
+drop table if exists Apply;
+
+/* -----------------------------------------------------------------------
+Better
+*/
+
+drop table if exists Student;
+drop table if exists Apply;
+drop table if exists HighSchool;
+
+create table Student (
+    SSN    int,
+    sName  text);
+
+create table Apply (
+    SSN    int,
+    cName  text,
+    hobby  text);
+
+create table HighSchool (
+    SSN    int,
+    HS     text,
+    HScity text);
+
+insert into Student values (123, 'Ann');
+insert into Student values (456, 'Bob');
+
+insert into Apply values (123, 'Berkeley', 'tennis');
+insert into Apply values (123, 'MIT',      'tennis');
+insert into Apply values (123, 'Stanford', 'tennis');
+insert into Apply values (123, 'Berkeley', 'tennis');
+insert into Apply values (123, 'MIT',      'tennis');
+insert into Apply values (123, 'Stanford', 'trumpet');
+insert into Apply values (123, 'Berkeley', 'trumpet');
+insert into Apply values (123, 'MIT',      'trumpet');
+insert into Apply values (123, 'Stanford', 'trumpet');
+insert into Apply values (123, 'Berkeley', 'trumpet');
+insert into Apply values (123, 'MIT',      'trumpet');
+insert into Apply values (456, 'MIT',      'surfing');
+
+insert into HighSchool values (123, 'PAHS', 'Palo Alto');
+insert into HighSchool values (123, 'GHS',  'Palo Alto');
+insert into HighSchool values (456, 'GHS',  'Palo Alto');
+
+drop table if exists Student;
+drop table if exists Apply;
+drop table if exists HighSchool;
+
+/* -----------------------------------------------------------------------
+Boyce-Codd Normal Form: BCNF
+Functional Dependencies
+SSN -> sName
+*/
+
+drop table if exists Apply;
+
+create table Apply (
+    SSN   int,
+    sName text,
+    cName text);
+
+insert into Apply values (123, 'Ann', 'Berkeley');
+insert into Apply values (123, 'Ann', 'MIT');
+insert into Apply values (123, 'Ann', 'Stanford');
+insert into Apply values (456, 'Bob', 'MIT');
+
+drop table if exists Apply;
+
+/* -----------------------------------------------------------------------
+BCNF
+No Functional Dependencies
+*/
+
+drop table if exists Student;
+drop table if exists Apply;
+
+create table Student (
+    SSN   int,
+    sName text);
+
+create table Apply (
+    SSN   int,
+    cName text);
+
+insert into Student values (123, 'Ann');
+insert into Student values (456, 'Bob');
+
+insert into Apply values (123, 'Berkeley');
+insert into Apply values (123, 'MIT');
+insert into Apply values (123, 'Stanford');
+insert into Apply values (456, 'MIT');
+
+drop table if exists Student;
+drop table if exists Apply;
+
+/* -----------------------------------------------------------------------
+4th Normal Form
+No Functional Dependencies
+SSN -/> cName
+SSN -/> HS
+
+Multivalued Dependencies
+Redundancy
+Multiplicative Effect: C colleges, H high schools results in C*H tuples
+SSN ->> cName
+SSN ->> HS
+*/
+
+drop table if exists Apply;
+
+create table Apply (
+    SSN   int,
+    cName text,
+    HS    text);
+
+insert into Apply values (123, 'Berkeley', 'PAHS');
+insert into Apply values (123, 'MIT',      'PAHS');
+insert into Apply values (123, 'Stanford', 'PAHS');
+insert into Apply values (123, 'Berkeley', 'GHS');
+insert into Apply values (123, 'MIT',      'GHS');
+insert into Apply values (123, 'Stanford', 'GHS');
+insert into Apply values (456, 'MIT',      'GHS');
+
+drop table if exists Apply;
+
+/* -----------------------------------------------------------------------
+4th Normal Form
+No Functional Dependencies
+No Multivalued Dependencies
+C+H, not C*H tuples
+*/
+
+drop table if exists Apply;
+drop table if exists HighSchool;
+
+create table Apply (
+    SSN   int,
+    cName text);
+
+create table HighSchool (
+    SSN   int,
+    HS    text);
+
+insert into Apply values (123, 'Berkeley');
+insert into Apply values (123, 'MIT');
+insert into Apply values (123, 'Stanford');
+insert into Apply values (456, 'MIT');
+
+insert into HighSchool values (123, 'PAHS');
+insert into HighSchool values (123, 'GHS');
+insert into HighSchool values (456, 'GHS');
+
+drop table if exists Apply;
+drop table if exists HighSchool;
+
+exit
