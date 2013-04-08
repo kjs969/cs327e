ac9d99af75d55cc7ff7c94509693cc2345e49f82
Glenn Downing <downing@cs.utexas.edu>
2013-04-08 12:08:54 -0500

    another commit

---
+++ quizzes/Quiz28.sql
@@ -0,0 +1,16 @@
+/*
+CS327e: Quiz #28 (5 pts)
+*/
+
+create table Student (sID   int,  sName text,    GPA        float, sizeHS   int);
+create table Apply   (sID   int,  cName text,    major      text,  decision boolean);
+create table College (cName text, state char(2), enrollment int);
+
+/* -----------------------------------------------------------------------
+1. Write a query to obtain number of applicants to each college.
+   (4 pts)
+*/
+
+select cName, count(distinct sID)
+    from Apply
+    group by cName;
