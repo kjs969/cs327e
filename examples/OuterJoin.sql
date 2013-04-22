9ea1bd9b4c183fcc076761c2986620d6e496ca0e
Glenn Downing <downing@cs.utexas.edu>
2013-04-22 08:43:25 -0500

    another commit

--- examples/OuterJoin.sql
+++ examples/OuterJoin.sql
@@ -81,26 +81,11 @@ name and major of students
 */
 
 select distinct sName, major
-    from Student, Apply
-    where Student.sID = Apply.sID
-    order by sName;
-
-select distinct sName, major
-    from Student inner join Apply
-    on Student.sID = Apply.sID
-    order by sName;
-
-select distinct sName, major
-    from Student join Apply
-    on Student.sID = Apply.sID
-    order by sName;
-
-select distinct sName, major
     from Student natural join Apply
     order by sName;
 
 select distinct sName, major
-    from Student join Apply
+    from Student inner join Apply
     using (sID)
     order by sName;
 
@@ -108,37 +93,6 @@ select distinct sName, major
 name and GPA of students with high school size less than 1000 and who
 applied to Stanford in CS
 */
-
-select sName, GPA
-    from Student, Apply
-    where
-        (Student.sID = Apply.sID)   and
-        (sizeHS      < 1000)        and
-        (major       = 'CS')        and
-        (cName       = 'Stanford');
-
-select sName, GPA
-    from Student inner join Apply
-    on Student.sID = Apply.sID
-    where
-        (sizeHS < 1000)        and
-        (major  = 'CS')        and
-        (cName  = 'Stanford');
-
-select sName, GPA
-    from Student join Apply
-    on Student.sID = Apply.sID
-    where
-        (sizeHS < 1000)        and
-        (major  = 'CS')        and
-        (cName  = 'Stanford');
-
-select sName, GPA
-    from Student natural join Apply
-    where
-        (sizeHS < 1000)        and
-        (major  = 'CS')        and
-        (cName  = 'Stanford');
 
 select sName, GPA
     from Student inner join Apply
@@ -152,25 +106,6 @@ select sName, GPA
 ID, name, GPA, college, and enrollment of all applications
 */
 
-select Student.sID, sName, GPA, Apply.cName, enrollment
-    from Student, Apply, College
-    where (Student.sID = Apply.sID) and (Apply.cName = College.cName)
-    order by Student.sID;
-
-select Student.sID, sName, GPA, Apply.cName, enrollment
-    from Student inner join Apply inner join College
-    on (Student.sID = Apply.sID) and (Apply.cName = College.cName)
-    order by Student.sID;
-
-select Student.sID, sName, GPA, Apply.cName, enrollment
-    from Student join Apply join College
-    on (Student.sID = Apply.sID) and (Apply.cName = College.cName)
-    order by Student.sID;
-
-select sID, sName, GPA, cName, enrollment
-    from Student natural join Apply natural join College
-    order by sID;
-
 select sID, sName, GPA, cName, enrollment
     from Student
         inner join Apply   using (sID)
@@ -180,31 +115,6 @@ select sID, sName, GPA, cName, enrollment
 /* -----------------------------------------------------------------------
 ID, name, and GPA of pairs of students with the same GPA
 */
-
-select R.sID, R.sName, R.GPA, S.sID, S.sName, S.GPA
-    from Student as R, Student as S
-    where (R.GPA = S.GPA) and (R.sID < S.sID)
-    order by R.sID;
-
-select R.sID, R.sName, R.GPA, S.sID, S.sName, S.GPA
-    from Student as R inner join Student as S
-    on (R.GPA = S.GPA) and (R.sID < S.sID)
-    order by R.sID;
-
-select R.sID, R.sName, R.GPA, S.sID, S.sName, S.GPA
-    from Student as R join Student as S
-    on (R.GPA = S.GPA) and (R.sID < S.sID)
-    order by R.sID;
-
-select sID1 as sID, sName1 as sName, GPA, sID2 as sID, sName2 as sName, GPA
-    from
-    (select sID as sID1, sName as sName1, GPA, sizeHS as sizeHS1
-        from Student) as R
-    natural join
-    (select sID as sID2, sName as sName2, GPA, sizeHS as sizeHS2
-        from Student) as S
-    where (sID1 < sID2)
-    order by sID1;
 
 select R.sID, R.sName, R.GPA, S.sID, S.sName, S.GPA
     from Student as R inner join Student as S
@@ -226,20 +136,6 @@ name, ID, college, and major of students who applied or not
 */
 
 select sID, sName, cName, major
-    from Student left outer join Apply
-    using (sID)
-    order by sID;
-
-select sID, sName, cName, major
-    from Student left join Apply
-    using (sID)
-    order by sID;
-
-select sID, sName, cName, major
-    from Student natural left outer join Apply
-    order by sID;
-
-select sID, sName, cName, major
     from Student inner join Apply
     using (sID)
 union
@@ -248,6 +144,11 @@ select sID, sName, null, null
     where sID not in
         (select sID from Apply)
 order by sID;
+
+select sID, sName, cName, major
+    from Student left join Apply
+    using (sID)
+    order by sID;
 
 /* -----------------------------------------------------------------------
 name, ID, college, and major of applications who are students or not
@@ -267,10 +168,10 @@ select sID, sName, cName, major
 name, ID, college, and major of students or not and applications or not
 */
 
-# mysql does not support intersect
+# mysql does not support full join
 
 # select sID, sName, cName, major
-#     from Student full outer join Apply
+#     from Student full join Apply
 #     using (sID)
 #     order by sID;
 
@@ -281,21 +182,6 @@ union
 select sID, sName, cName, major
     from Student right join Apply
     using (sID)
-order by sID;
-
-select sID, sName, cName, major
-    from Student inner join Apply
-    using (sID)
-union
-select sID, sName, null, null
-    from Student
-    where sID not in
-        (select sID from Apply)
-union
-select sID, null, cName, major
-    from Apply
-    where sID not in
-        (select sID from Student)
 order by sID;
 
 /* -----------------------------------------------------------------------
